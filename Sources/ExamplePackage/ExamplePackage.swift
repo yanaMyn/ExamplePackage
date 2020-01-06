79dfea9807c3f6361f24409b82dcdb1c0a2f5fb7
yana mulyana <yanamyn15@gmail.com>
2020-01-06 23:02:02 +0700

    remove struct & add class Animal

--- Sources/ExamplePackage/ExamplePackage.swift
+++ Sources/ExamplePackage/ExamplePackage.swift
@@ -1,7 +1,5 @@
-public struct ExamplePackage {
-    public var text = "Hello, World!"
-}
-
-public struct Example {
-    public var text = "Hello, Example! tag 1.0.1"
+public class Animal {
+    public func cat() {
+        print("miaw")
+    }
 }
