e233fd6304c27c1e47c7c00b189888c534ddcbb2
yana mulyana <yanamyn15@gmail.com>
2020-01-23 14:42:09 +0700

    add network

---
+++ Sources/Network/Network.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by yana mulyana on 23/01/20.
+//
+
+import Foundation
+
+public class Network {
+    
+    
+    public init() {
+        print("Network initializa")
+    }
+    
+    public func request() {
+        print("start request")
+    }
+}
